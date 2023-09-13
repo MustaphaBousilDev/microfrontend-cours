2e90da68fb6d05f651a23bbd5b5c034d54068735
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-13 14:00:09 +0100

    Generec Integration between Marketing Component and COntainer compoentn

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { mount } from 'marketing/MarketingApp'
-
-console.log(mount)
+import MarketingApp from './components/MarketingApp';
 
 export default ()=>{
-    return <h1>Hi mother fucker</h1>
+    return (
+        <>
+        <div>
+            <h1>Hi mother fucker</h1>
+            <hr/>
+            <MarketingApp/>
+        </div>
+        </>
+    )
 }
