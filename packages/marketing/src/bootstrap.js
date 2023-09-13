7576857b2bea0f8d4ee6a39d5e8443ea9ee2565b
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-13 10:40:35 +0100

    Assembling the App Container

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App';
 
 
 //mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>Hi There!</h1>,
-        el
-    )
+    ReactDOM.render(<App/>,el)
 }
 
 
