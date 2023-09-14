f6ba9cd5143239712bdb480ff512d66b4ee3411f
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-14 14:54:12 +0100

    delegating shared model selection

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin= require('html-webpack-plugin')
 const commonConfig=require('./webpack.common')
+const packageJson=require('../package.json')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const devConfig = {
@@ -17,7 +18,7 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js',
             },
-            shared: ['react','react-dom']
+            shared: packageJson.dependencies
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
